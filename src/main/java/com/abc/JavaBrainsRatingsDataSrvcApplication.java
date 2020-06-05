93c3f8abef513d999eb8e5c1e7e6a8ed8e050086
rishiwasdev <rishiwasdev@gmail.com>
2020-06-05 08:09:12 +0530

    Done: microservice communication, Eureka implementation, remote service (TheMovieDB APIs) integration

--- src/main/java/com/abc/JavaBrainsRatingsDataSrvcApplication.java
+++ src/main/java/com/abc/JavaBrainsRatingsDataSrvcApplication.java
@@ -3,6 +3,7 @@ package com.abc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -14,6 +15,7 @@ import com.abc.config.AuditorAwareImpl;
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableEurekaClient
 public class JavaBrainsRatingsDataSrvcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBrainsRatingsDataSrvcApplication.class, args);
